c26ac31fc4c1b42ea5730058b35ffcbb438ae5b3
hendra15 <hendrawanagus15@gmail.com>
2021-12-07 10:02:30 +0700

    perbaikan responsiv

--- resources/views/template/header_footer.blade.php
+++ resources/views/template/header_footer.blade.php
@@ -107,7 +107,7 @@
             </div>
             <div class="page-container">
                 <div class="header navbar">
-                    <div class="header-container">
+                    <div class="header-container col-sm-12">
                         <ul class="nav-left">
                             <li><a id="sidebar-toggle" class="sidebar-toggle" href="javascript:void(0);"><i class="ti-menu"></i></a>
                             </li>
@@ -120,7 +120,7 @@
                             <li class="dropdown">
                                 <a href="" class="dropdown-toggle no-after peers fxw-nw ai-c lh-1" data-toggle="dropdown">
                                     <div class="peer mR-10"><img class="w-2r bdrs-50p" src="{{asset('template/images/janti.jpeg')}}" alt=""></div>
-                                    <div class="peer"><span class="fsz-sm c-grey-900">{{(Auth::user()->type == 2)?Auth::user()->user_id:Auth::user()->dosen->name}}</span></div>
+                                    <div class="peer"><span class="fsz-sm c-grey-900 p-3">{{(Auth::user()->type == 2)?Auth::user()->user_id:Auth::user()->dosen->name}}</span></div>
                                 </a>
                                 <ul class="dropdown-menu fsz-sm">
                                     <li><a href="{{route('profil')}}" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700"><i class="fas fa-user-circle mR-10"></i>
